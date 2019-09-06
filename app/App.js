363e3f821e5a62b407a877240a555e580761812c
AM77 <amine.griche77@gmail.com>
2019-09-06 17:32:48 +0200

    renamed app.js->App.js & added controller to users-comp

--- app/App.js
+++ app/App.js
@@ -3,18 +3,35 @@ export default class App {
         this.root = document.querySelector(_root)
         this.components = []
 
+        this.show_comp = this.show_comp.bind(this)
     }
 
     add_comp(_comp) {
-        this.components[_comp.id] = _comp
+        this.components.push(_comp)
     }
 
-    show_comp() {
+    show_comp(_route) {
+
+        for (let i = 0; i < this.components.length; i++) {
+            if (this.components[i].id == _route.comp_id) {
+                this.current_comp = this.components[i]
+                break
+            } else {
+                this.current_comp = {
+                    view() {
+                        return `<h4>404 NOT FOUND</h4>`
+                    }
+                }
+            }
+        }
+
+        this.current_comp.controller()
+        this.update_html(this.current_comp)
 
     }
 
-    update_view() {
-
+    update_html(_comp) {
+        this.root.innerHTML = _comp.view()
     }
 
 }
