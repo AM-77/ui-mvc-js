7829ab80f50720c9923af3cf06d011a0678cba05
AM77 <amine.griche77@gmail.com>
2019-09-06 18:05:37 +0200

    updated hashchanged()

--- app/Router.js
+++ app/Router.js
@@ -34,7 +34,21 @@ export default class Router {
             }
         }
 
-        this.app.show_comp(this.current_route)
+        for (let i = 0; i < this.app.components.length; i++) {
+            if (this.app.components[i].id == this.current_route.comp_id) {
+                this.current_comp = this.app.components[i]
+                break
+            } else {
+                this.current_comp = {
+                    view() {
+                        return `<h4>404 NOT FOUND</h4>`
+                    },
+                    controller() {}
+                }
+            }
+        }
 
+        this.current_comp.controller()
+        this.app.show_comp(this.current_comp)
     }
 }
