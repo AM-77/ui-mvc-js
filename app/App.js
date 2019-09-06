a8136daae0b299f1070ce9ff5d238cd2bcf9e89f
AM77 <amine.griche77@gmail.com>
2019-09-06 18:05:04 +0200

    removed update_html()

--- app/App.js
+++ app/App.js
@@ -10,27 +10,7 @@ export default class App {
         this.components.push(_comp)
     }
 
-    show_comp(_route) {
-
-        for (let i = 0; i < this.components.length; i++) {
-            if (this.components[i].id == _route.comp_id) {
-                this.current_comp = this.components[i]
-                break
-            } else {
-                this.current_comp = {
-                    view() {
-                        return `<h4>404 NOT FOUND</h4>`
-                    }
-                }
-            }
-        }
-
-        this.current_comp.controller()
-        this.update_html(this.current_comp)
-
-    }
-
-    update_html(_comp) {
+    show_comp(_comp) {
         this.root.innerHTML = _comp.view()
     }
 
